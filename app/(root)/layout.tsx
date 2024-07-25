e9553abadde1a883d819c6f2473f56b09e5d6fc1
Benjamin J Hultin <benhultin@BenjaminsLaptop.lan>
2024-07-24 20:23:18 -0700

    Added clerk for authentication

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,15 @@
+import { SignedIn } from '@clerk/nextjs'
 import React from 'react'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root" >
       <div className="root-container">
-        <div className="wrapper">
-          {children}
-        </div>
+        <SignedIn>
+          <div className="wrapper">
+            {children}
+          </div>
+        </SignedIn>
       </div>
     </main>
   )
